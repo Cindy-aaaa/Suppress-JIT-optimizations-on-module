bad8104ef0dff4a0222418f1ac4181c3c6b3a5b8
Judy Zhu (CSI Interfusion Inc) <v-czhu@microsoft.com>
2020-11-11 10:50:55 -0800

    注释

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Class1 cl = new Class1();
+
+			cl.SurpressJit(); //set bp here
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
